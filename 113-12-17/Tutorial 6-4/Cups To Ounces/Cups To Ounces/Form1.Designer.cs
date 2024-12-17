52d66fd29d6cbaa74cde1ee160045921713f70f0
Shuo Cheng Hu <shuochenghu@gmail.com>
2024-12-17 18:02:57 +0800

    113-12-17

---
+++ 113-12-17/Tutorial 6-4/Cups To Ounces/Cups To Ounces/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Cups_To_Ounces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bannerLabel = new System.Windows.Forms.Label();
+            this.cupsPromptLabel = new System.Windows.Forms.Label();
+            this.cupsTextBox = new System.Windows.Forms.TextBox();
+            this.ouncesLabel = new System.Windows.Forms.Label();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bannerLabel
+            // 
+            this.bannerLabel.AutoSize = true;
+            this.bannerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bannerLabel.Location = new System.Drawing.Point(20, 14);
+            this.bannerLabel.Name = "bannerLabel";
+            this.bannerLabel.Size = new System.Drawing.Size(135, 16);
+            this.bannerLabel.TabIndex = 0;
+            this.bannerLabel.Text = "杯至液體盎轉換器";
+            // 
+            // cupsPromptLabel
+            // 
+            this.cupsPromptLabel.AutoSize = true;
+            this.cupsPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.cupsPromptLabel.Location = new System.Drawing.Point(94, 43);
+            this.cupsPromptLabel.Name = "cupsPromptLabel";
+            this.cupsPromptLabel.Size = new System.Drawing.Size(22, 13);
+            this.cupsPromptLabel.TabIndex = 1;
+            this.cupsPromptLabel.Text = "杯:";
+            // 
+            // cupsTextBox
+            // 
+            this.cupsTextBox.Location = new System.Drawing.Point(134, 41);
+            this.cupsTextBox.Name = "cupsTextBox";
+            this.cupsTextBox.Size = new System.Drawing.Size(74, 22);
+            this.cupsTextBox.TabIndex = 2;
+            // 
+            // ouncesLabel
+            // 
+            this.ouncesLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ouncesLabel.Location = new System.Drawing.Point(134, 66);
+            this.ouncesLabel.Name = "ouncesLabel";
+            this.ouncesLabel.Size = new System.Drawing.Size(74, 21);
+            this.ouncesLabel.TabIndex = 3;
+            this.ouncesLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(72, 70);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(44, 12);
+            this.outputDescriptionLabel.TabIndex = 4;
+            this.outputDescriptionLabel.Text = "液體盎:";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(54, 108);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 21);
+            this.convertButton.TabIndex = 5;
+            this.convertButton.Text = "轉換";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(135, 108);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 21);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 142);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.ouncesLabel);
+            this.Controls.Add(this.cupsTextBox);
+            this.Controls.Add(this.cupsPromptLabel);
+            this.Controls.Add(this.bannerLabel);
+            this.Name = "Form1";
+            this.Text = "杯轉盎";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label bannerLabel;
+        private System.Windows.Forms.Label cupsPromptLabel;
+        private System.Windows.Forms.TextBox cupsTextBox;
+        private System.Windows.Forms.Label ouncesLabel;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
---
+++ 113-12-17/Tutorial 6-5/Pay and Bonus/Pay and Bonus/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pay_and_Bonus
+{
+    public partial class Form1 : Form
+    {
+        // Constant field for the contribution rate
+        private const decimal CONTRIB_RATE = 0.05m;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The InputIsValid method converts the user input and stores
+        // it in the arguments (passed by reference). If the conversion
+        // is successful, the method returns true. Otherwise it returns
+        // false.
+        
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal grossPay = 0.0m;   // 用於保存總薪資
+            decimal bonus = 0.0m;      // 用於保存獎金
+
+            if (InputIsValid(ref grossPay, ref bonus))
+            {
+                decimal contribution = (grossPay + bonus) * CONTRIB_RATE;   // 用於保存員工的貢獻
+                contributionLabel.Text = contribution.ToString("c");        // 顯示貢獻
+            }
+            else
+            {
+                // Display an error message for invalid input.
+                MessageBox.Show("請輸入有效的數字");
+            }
+
+        }
+
+        private bool InputIsValid(ref decimal grossPay, ref decimal bonus)
+        {
+            // 標誌變數，用於指示輸入是否有效
+            bool inputGood = false;
+
+            // 嘗試將總薪資文本框轉換為十進制
+            if (decimal.TryParse(grossPayTextBox.Text, out grossPay))
+            {
+                // 嘗試將獎金文本框轉換為十進制
+                if (decimal.TryParse(bonusTextBox.Text, out bonus))
+                {
+                    // 兩個輸入都有效
+                    inputGood = true;
+                }
+            }
+
+            // 返回結果
+            return inputGood;
+        }
+
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
